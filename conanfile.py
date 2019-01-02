821bbfb6328a44cd864d627ad02c34450517c371
Uilian Ries <uilianries@gmail.com>
2019-01-02 13:30:48 -0200

    Install system requirements
    - libui requires many libraries, including gnome ones. Sice we don't
      have all proper packages, the only option now is installing by
      the system package manager. We should re-visit this one when all
      dependencies are available.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class libuiConan(ConanFile):
     url = "https://github.com/bincrafters/conan-libui"
     homepage = "https://github.com/andlabs/libui"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT" 
+    license = "MIT"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
@@ -25,28 +25,24 @@ class libuiConan(ConanFile):
     _build_subfolder = "build_subfolder"
 
     def system_requirements(self):
-        if os_info.linux_distro == "ubuntu":
-            # pack_names = [
-                # "libgtk-3-dev",
-                # "libgirepository-1.0-1:i386
-                # "libglib2.0-dev:i386
-                # "gir1.2-glib-2.0:i386
-                # "gir1.2-atk-1.0:i386
-                # "libatk1.0-dev:i386
-                # "libfreetype6-dev:i386
-                # "libfontconfig1-dev:i386
-                # "libcairo2-dev:i386
-                # "libgdk-pixbuf2.0-dev:i386
-                # "libpango1.0-dev:i386
-                # "libxft-dev:i386
-                # "libpng12-dev:i386
-            # ]
+        if tools.os_info.with_apt:
+            packages = [
+                "libgtk-3-dev",
+                "libatk1.0-dev",
+                "libglib2.0-dev",
+                "libpango1.0-dev",
+                "libgdk-pixbuf2.0-dev",
+                "libcairo2-dev",
+                "libpango1.0-dev",
+                "libcairo2-dev"
+            ]
+            if tools.cross_building(self.settings):
+                target_arch = {"x86": "i386", "armv7": "arm", "armv7hf": "armhf"}
+                conan_arch = str(self.settings.arch)
+                packages = ["%s:%s" % (package, target_arch[conan_arch]) for package in packages]
             installer = SystemPackageTool()
-            installer.install("libgtk-3-dev")
-            
-        
-        
-        
+            installer.install(" ".join(packages))
+
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
@@ -54,7 +50,7 @@ class libuiConan(ConanFile):
     def source(self):
         url_version = "alpha4.1"
         tools.get(
-            "{0}/archive/{1}.tar.gz".format(self.homepage, url_version), 
+            "{0}/archive/{1}.tar.gz".format(self.homepage, url_version),
             sha256="f51a9e20e9f9a4c0bce1571ee37f203f42de33e3ac7359a6aac87a54798e8716")
         extracted_dir = self.name + "-" + url_version
         os.rename(extracted_dir, self._source_subfolder)
@@ -109,6 +105,5 @@ class libuiConan(ConanFile):
                 "gobject-2.0",
                 "glib-2.0",
             ])
-            
-            
-            
+
+
